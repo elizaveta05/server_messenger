4c1af87cb7166d38107bfe3970e6ce0486ba2840
elizaveta05 <elozovetalyapynova05@gmail.com>
2024-10-02 19:51:33 +0700

    first commit

---
+++ src/test/java/com/example/server_messenger/ServerMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
